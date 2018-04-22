2440d1b4af10e9442e844fd03a70a50b04282610
ovk2015 <ovk.aggregator@gmail.com>
2018-04-22 23:54:40 +0300

    Add existing file

--- chatServer.js
+++ chatServer.js
@@ -1,9 +1,24 @@
-var webSocket = require('ws'),
-	server = new webSocket.Server({port:5000});
+var pport = process.env.PORT || 5000;
+	webSocket = require('ws'),
+	server = new webSocket.Server({port:pport});
 
 server.on('connection', function(ws)
 {
 	ws.send('Вы подключились к WS серверу');
 });
+
+server.listen(pport, function(err) 
+{
+    if(!err) 
+    { 
+    	console.log('Listening on port ' + pport); 
+    }
+    else
+    {
+    	console.log('Ошибка('); 	
+    	console.log(err); 	
+    }
+});
+
 
 console.log('Вроде работает!');
