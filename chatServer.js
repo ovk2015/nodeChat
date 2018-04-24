d9c6579c1529376d8720b53301f63e1610d3c7d9
ovk2015 <ovk.aggregator@gmail.com>
2018-04-24 17:33:08 +0300

    Add existing file

--- chatServer.js
+++ chatServer.js
@@ -2,23 +2,88 @@ var pport = process.env.PORT || 5000;
 	webSocket = require('ws'),
 	server = new webSocket.Server({port:pport});
 
-server.on('connection', function(ws)
+var clients = [];
+
+server.on('connection', function(ws, url)
 {
-	ws.send('Вы подключились к WS серверу');
+
+	var clientName = url.url.substr(1);
+    clients.push({name: clientName, socket: ws});
+    console.log('Подключился клиeнт ' + clientName);
+
+	ws.on('message', function(data)
+	{
+		data = JSON.parse(data);
+		clients
+		.filter(function (item)
+		{
+			return item.name == data.to;
+		})[0].socket
+		.send
+		(
+			JSON.stringify
+			({
+				sender: 'client',
+				type: 'message',
+				data:
+				{
+					from: data.from,
+					message: data.message
+				}
+			})
+		);
+		// console.log(clients);
+		// console.log(data);
+	});
+
+    server.clients.forEach(function(client)
+    {
+        // if(client !== ws && client.readyState === WebSocket.OPEN)
+        if(client !== ws && client.readyState === 1)
+        {
+            console.log('Отправляем сообщение остальным');
+            client.send
+            (
+            	JSON.stringify
+				({
+					sender: 'server',
+					type: 'info',
+					data:
+					{
+						user: clientName,
+						action: 'newContact'
+					}
+				})
+			);
+        }
+    });
+	ws.send
+	(
+		JSON.stringify
+		({
+			sender: 'server',
+			type: 'info',
+			data:
+			{
+				action: 'connected',
+				users: clients
+					.map(function(item){return item.name})
+					.filter(function(item){return item != clientName})
+			}
+		})
+	);
 });
 
-// server.listen(pport, function(err) 
-// {
-//     if(!err) 
-//     { 
-//     	console.log('Listening on port ' + pport); 
-//     }
-//     else
-//     {
-//     	console.log('Ошибка('); 	
-//     	console.log(err); 	
-//     }
-// });
+/*server.on('message', function(data)
+{
+	data = JSON.parse(data);
+	console.log(data);
+});*/
+
+server.on('close', function() 
+{
+	console.log('closing connection');
+});
 
 
 console.log('Вроде работает на порту ' + pport);
